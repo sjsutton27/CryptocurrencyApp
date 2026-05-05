3a59fbd8d8fd645ac7868e5c0558b17d53a4a041
sjsutton27 <ssutton101600@gmail.com>
2026-05-05 14:22:04 -0400

    Update CoinApplication.kt

--- app/src/main/java/com/example/cryptocurrencyappyt/CoinApplication.kt
+++ app/src/main/java/com/example/cryptocurrencyappyt/CoinApplication.kt
@@ -3,9 +3,6 @@ package com.example.cryptocurrencyappyt
 import android.app.Application
 import com.example.cryptocurrencyappyt.di.AppModule
 import com.example.cryptocurrencyappyt.di.AppModuleImpl
-//import com.example.cryptocurrencyappyt.di.appModule
-import timber.log.Timber
-
 class CoinApplication : Application(){
 
     companion object{
